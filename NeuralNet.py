b73d1469cff4ad4b00facc19783908334081348e
Floppy-code <jurajo80@gmail.com>
2020-11-17 16:59:16 +0100

    Added requirements and slight fixes to CNN

--- NeuralNet.py
+++ NeuralNet.py
@@ -9,23 +9,23 @@ def get_neural_net_WH():
     
     #PROOF OF CONCEPT MODEL
     model = Sequential()
-    model.add(Conv2D(8, (3,3), input_shape = NN_SHAPE, padding = 'same')) #Width, Height, Colors
+    model.add(Conv2D(16, (5,5), input_shape = NN_SHAPE, padding = 'same')) #Width, Height, Colors
     model.add(Activation('relu'))
-    model.add(Conv2D(16, (3,3), padding = 'same'))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size = (2,2)))
-
-    model.add(Conv2D(8, (3,3), padding = 'same'))
-    model.add(Activation('relu'))
-    model.add(Conv2D(16, (3,3), padding = 'same'))
+    model.add(Conv2D(32, (5,5), padding = 'same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
 
-    model.add(Conv2D(8, (3,3), padding = 'same'))
+    model.add(Conv2D(16, (5,5), padding = 'same'))
     model.add(Activation('relu'))
-    model.add(Conv2D(16, (3,3), padding = 'same'))
+    model.add(Conv2D(32, (5,5), padding = 'same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
+
+    model.add(Conv2D(16, (5,5), padding = 'same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (5,5), padding = 'same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size = (3,3)))
 
     model.add(Flatten())
 
